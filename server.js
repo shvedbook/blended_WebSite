a4d21362b502aa08cb56baf57607c135ff803403
Alex Golberg <shvedbook@gmail.com>
2020-05-09 10:50:18 +0300

    turn the website to express.js based

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (request, response) =>  {
-    response.send('Hello Express :)')
+    response.sendFile(path.join(__dirname, './static/index.html'))
+});
+
+app.get('/experts', (request, response) =>  {
+    response.sendFile(path.join(__dirname, './static/exprets.html'))
+});
+
+app.get('/casestudy', (request, response) =>  {
+    response.sendFile(path.join(__dirname, './static/casestudy.html'))
+});
+
+app.get('/models', (request, response) =>  {
+    response.sendFile(path.join(__dirname, './static/models.html'))
+});
+
+app.get('/tools', (request, response) =>  {
+    response.sendFile(path.join(__dirname, './static/tools.html'))
+});
+
+app.get('/forum', (request, response) =>  {
+    response.sendFile(path.join(__dirname, './static/forum.html'))
 });
 
 app.listen(port, () => {
